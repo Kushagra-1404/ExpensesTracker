e7a5dedd01238a94028b1cc9346d626d75c50fdd
Kushagra Gupta <guptakushagra1404@gmail.com>
2023-06-03 18:11:39 +0530

    added initial files

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,22 @@
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+
+const ExpenseItem = (props) => {
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date.getFullYear();
+  return (
+    <li>
+      <Card className="expense-item">
+        <ExpenseDate date={props.date} />
+        <div className="expense-item__description">
+          <h2>{props.title}</h2>
+          <div className="expense-item__price">{props.amount}</div>
+        </div>
+      </Card>
+    </li>
+  );
+};
+
+export default ExpenseItem;
